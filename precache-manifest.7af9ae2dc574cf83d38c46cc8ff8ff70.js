0f4ea0e485725a3c3075113c3eee46ac4cf57edb
borajin <55897222+borajin@users.noreply.github.com>
2020-04-19 19:15:19 +0900

    Updates

---
+++ precache-manifest.7af9ae2dc574cf83d38c46cc8ff8ff70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "816c278bbcb59adf1d9d6f798eaccffc",
+    "url": "/react-movie-app/index.html"
+  },
+  {
+    "revision": "e996a480f11ef87960a7",
+    "url": "/react-movie-app/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "36100056dc4435dc14c1",
+    "url": "/react-movie-app/static/js/2.fc5dc0a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-movie-app/static/js/2.fc5dc0a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e996a480f11ef87960a7",
+    "url": "/react-movie-app/static/js/main.70e4b3d8.chunk.js"
+  },
+  {
+    "revision": "869144972e65cdfd1086",
+    "url": "/react-movie-app/static/js/runtime-main.42ea3b02.js"
+  }
+]);
